86f55ae465f7368337975b4cc8eae0229db90d30
Ярослав Богданов <bogdanov.yar99@gmail.com>
2025-04-23 21:23:44 +0300

    fix 4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 
-const contactsPersistConfig = {
-  key: "contacts",
-  storage,
-  whitelist: ["items"],
-};
-
 const rootReducer = combineReducers({
-  contacts: persistReducer(contactsPersistConfig, contactsReducer),
+  contacts: contactsReducer,
   filters: filtersReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
 });
-
-export const persistor = persistStore(store);
